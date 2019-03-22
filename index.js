51c37c6a28488dd205a4c9415670c4803bc1cfb3
Mohamed Ben hartouz <benhartouzmohamed@gmail.com>
2019-03-22 18:04:18 +0100

    add cron

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import { getHTML, getCountInsta, getCountTweeter } from "./lib/scraper";
+import express from "express";
+import db from "./lib/db";
+import "./lib/cron";
 
-async function go() {
+const app = express();
+
+// Scraper
+app.get("/scrape", async (req, res) => {
     const [instaCount, tweeterCount] = await Promise.all([
         getCountInsta(),
         getCountTweeter()
     ]);
-    console.log("followers Twitter:", instaCount);
-    console.log("followers Instagram:", tweeterCount);
-}
+    // Add a instagram counts on current date
+    db.get("instagram")
+        .push({ date: Date.now(), count: instaCount })
+        .write();
+    db.get("tweeter")
+        .push({ date: Date.now(), count: tweeterCount })
+        .write();
+    res.json({ instaCount, tweeterCount });
+});
 
-go();
+app.listen(3333, () => {
+    console.log("Server run on port 3333");
+});
